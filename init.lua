27bc175aaf961fc5ca5e96c93f63d9510713ee01
MahdiGMK <mbahramiand@gmail.com>
2023-10-26 11:23:49 +0330

    add neotree

--- init.lua
+++ init.lua
@@ -218,8 +218,7 @@ require('lazy').setup({
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
-  require 'kickstart.plugins.autoformat',
-  require 'kickstart.plugins.debug',
+  { import = 'kickstart.plugins' },
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
