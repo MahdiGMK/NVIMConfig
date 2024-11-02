b16214d970842963719fe11d7e6a42c07891fc5d
MahdiGMK <mbahramiand@gmail.com>
2024-11-03 00:05:16 +0330

    finally fixed whichkey warning

--- init.lua
+++ init.lua
@@ -527,15 +527,15 @@ local on_attach = function(client, bufnr)
 end
 
 -- document existing key chains
-require('which-key').register {
-    ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-    ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-    ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-    ['<leader>h'] = { name = 'More git/Harpoon', _ = 'which_key_ignore' },
-    ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-    ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-    ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-    ['<leader>t'] = { name = '[T]abs', _ = 'which_key_ignore' }
+require('which-key').add {
+    { "<leader>c", group = "[C]ode" },
+    { "<leader>d", group = "[D]ocument" },
+    { "<leader>g", group = "[G]it" },
+    { "<leader>h", group = "More git/Harpoon" },
+    { "<leader>r", group = "[R]ename" },
+    { "<leader>s", group = "[S]earch" },
+    { "<leader>t", group = "[T]abs" },
+    { "<leader>w", group = "[W]orkspace" },
 }
 
 -- mason-lspconfig requires that these setup functions are called in this order
